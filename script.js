c285229e69221a5d18e58875f0527799ff0087ce
VivianaSalazarIesBelen <vsallug630@g.educaand.es>
2026-06-02 08:41:04 +0200

    ejercicio actualizado

--- script.js
+++ script.js
@@ -1,1 +1,136 @@
-console.log('Happy developing ✨')
+import { datosUE } from "./uecountries.js";
+
+// Estado global para recordar las selecciones
+let idiomaSeleccionado = "todos";
+let soloOficiales = false;
+
+// Clases de Bootstrap para pintar los botones en horizontal con bordes
+const ESTILO_BOTON = "d-inline-flex align-items-center gap-1 border border-secondary rounded px-2 py-1 mb-1 me-1";
+
+// Al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+    generarIdiomas();
+    configurarFiltros();
+    render();
+});
+
+// Motor que redibuja la interfaz
+function render() {
+    let lista = [...datosUE];
+
+    // Orden siempre descendente por población
+    lista.sort((a, b) => b.poblacion_nacional - a.poblacion_nacional);
+
+    // Pasamos los datos por el embudo de filtros
+    lista = aplicarFiltros(lista);
+
+    // Pintamos los elementos resultantes en el HTML
+    pintarTabla(lista);
+    pintarInfo(lista);
+}
+
+// Aplica las condiciones de filtrado
+function aplicarFiltros(lista) {
+    if (idiomaSeleccionado === "todos") return lista;
+
+    return lista.filter(pais => {
+        const oficial = pais.idiomas.oficial?.toLowerCase() || "";
+        const otros = pais.idiomas.otros_idiomas?.toLowerCase() || "";
+        const idioma = idiomaSeleccionado.toLowerCase();
+
+        // Convertimos los strings separados por comas en arrays limpios
+        const matchOficial = oficial.split(",").map(i => i.trim()).includes(idioma);
+        const matchOtros = otros.split(",").map(i => i.trim()).includes(idioma);
+
+        if (soloOficiales) {
+            return matchOficial; // Si el checkbox está activo, solo cuenta si es oficial
+        }
+        return matchOficial || matchOtros; // Si no, cuenta si es oficial u otros
+    });
+}
+
+// Genera las filas de la tabla de países
+function pintarTabla(lista) {
+    const contenedor = document.getElementById("tabla-paises");
+
+    contenedor.innerHTML = lista.map(p => {
+        const esMonarquia = p.regimen_politico?.tipo?.toLowerCase()?.includes("monarquía");
+        const nombrePais = esMonarquia ? `${p.pais} 👑` : p.pais;
+        const fecha = new Date(p.fecha_adhesion).toLocaleDateString("es-ES");
+
+        return `
+            <tr>
+                <td>${nombrePais}</td>
+                <td>${p.capital}</td>
+                <td>${p.poblacion_nacional.toLocaleString("es-ES")}</td>
+                <td>${fecha}</td>
+            </tr>
+        `;
+    }).join("");
+}
+
+// Recolecta los idiomas únicos y crea los botones de radio horizontales
+function generarIdiomas() {
+    const set = new Set();
+
+    datosUE.forEach(p => {
+        if (p.idiomas.oficial) {
+            p.idiomas.oficial.split(",").forEach(i => set.add(i.trim().toLowerCase()));
+        }
+        if (p.idiomas.otros_idiomas) {
+            p.idiomas.otros_idiomas.split(",").forEach(i => set.add(i.trim().toLowerCase()));
+        }
+    });
+
+    const idiomas = [...set].filter(i => i && i !== "null").sort();
+    const cont = document.getElementById("radios-idiomas");
+
+    // Primer botón horizontal por defecto: todos
+    let htmlContent = `
+        <div class="${ESTILO_BOTON}">
+            <input type="radio" name="idioma" id="lang-todos" value="todos" checked>
+            <label for="lang-todos">todos</label>
+        </div>
+    `;
+
+    // Añadimos el resto de idiomas con la clase de borde de tu compañero
+    idiomas.forEach((i, index) => {
+        htmlContent += `
+            <div class="${ESTILO_BOTON}">
+                <input type="radio" name="idioma" id="lang-${index}" value="${i}">
+                <label class="text-capitalize" for="lang-${index}">${i}</label>
+            </div>
+        `;
+    });
+
+    cont.innerHTML = htmlContent;
+}
+
+// Configura los escuchadores de eventos
+function configurarFiltros() {
+    // Escucha el cambio del CHECKBOX
+    document.getElementById("solo-oficiales").addEventListener("change", (e) => {
+        soloOficiales = e.target.checked;
+        render();
+    });
+
+    // Escucha el cambio de los RADIOS horizontales
+    document.getElementById("radios-idiomas").addEventListener("change", (e) => {
+        if (e.target.name === "idioma") {
+            idiomaSeleccionado = e.target.value;
+            render();
+        }
+    });
+}
+
+// Imprime el texto de control inferior
+function pintarInfo(lista) {
+    const cont = document.getElementById("info-filtro");
+
+    if (idiomaSeleccionado === "todos") {
+        cont.innerHTML = `Se muestran ${lista.length} países de la UE`;
+    } else {
+        const tipo = soloOficiales ? "oficial" : "oficial u otros";
+        cont.innerHTML = `Filtrado por: "${idiomaSeleccionado}" (${tipo}) (${lista.length} de ${datosUE.length})`;
+    }
+}
---
+++ uecountries.js
@@ -0,0 +1,470 @@
+export const datosUE = [
+    {
+        "pais": "Alemania",
+        "poblacion_nacional": 83600000,
+        "capital": "Berlín",
+        "ciudades_principales": [
+            {"nombre": "Berlín", "poblacion": 3520000},
+            {"nombre": "Hamburgo", "poblacion": 1790000},
+            {"nombre": "Múnich", "poblacion": 1450000}
+        ],
+        "idiomas": {
+            "oficial": "Alemán",
+            "otros_idiomas": "Danés, bajo alemán"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria federal",
+            "jefe_estado": "Presidente Frank-Walter Steinmeier"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Austria",
+        "poblacion_nacional": 9660000,
+        "capital": "Viena",
+        "ciudades_principales": [
+            {"nombre": "Viena", "poblacion": 1900000}
+        ],
+        "idiomas": {
+            "oficial": "Alemán",
+            "otros_idiomas": "Croata, húngaro, esloveno"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Alexander Van der Bellen"
+        },
+        "fecha_adhesion": "1995-01-01"
+    },
+    {
+        "pais": "Bélgica",
+        "poblacion_nacional": 11880000,
+        "capital": "Bruselas",
+        "ciudades_principales": [
+            {"nombre": "Bruselas", "poblacion": 1200000}
+        ],
+        "idiomas": {
+            "oficial": "Neerlandés, francés, alemán",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional federal",
+            "jefe_estado": "Rey Felipe (Philippe)"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Bulgaria",
+        "poblacion_nacional": 6700000,
+        "capital": "Sofía",
+        "ciudades_principales": [
+            {"nombre": "Sofía", "poblacion": 1200000}
+        ],
+        "idiomas": {
+            "oficial": "Búlgaro",
+            "otros_idiomas": "Turco, romaní"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Rumen Radev"
+        },
+        "fecha_adhesion": "2007-01-01"
+    },
+    {
+        "pais": "Chequia",
+        "poblacion_nacional": 10880000,
+        "capital": "Praga",
+        "ciudades_principales": [
+            {"nombre": "Praga", "poblacion": 1300000}
+        ],
+        "idiomas": {
+            "oficial": "Checo",
+            "otros_idiomas": "Eslovaco"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Petr Pavel"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Chipre",
+        "poblacion_nacional": 920000,
+        "capital": "Nicosia",
+        "ciudades_principales": [
+            {"nombre": "Nicosia", "poblacion": 330000}
+        ],
+        "idiomas": {
+            "oficial": "Griego, turco",
+            "otros_idiomas": "Inglés"
+        },
+        "regimen_politico": {
+            "tipo": "República presidencialista",
+            "jefe_estado": "Presidente Nikos Christodoulides"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Croacia",
+        "poblacion_nacional": 4100000,
+        "capital": "Zagreb",
+        "ciudades_principales": [
+            {"nombre": "Zagreb", "poblacion": 760000}
+        ],
+        "idiomas": {
+            "oficial": "Croata",
+            "otros_idiomas": "Serbio, italiano, húngaro"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Zoran Milanović"
+        },
+        "fecha_adhesion": "2013-07-01"
+    },
+    {
+        "pais": "Dinamarca",
+        "poblacion_nacional": 5790000,
+        "capital": "Copenhague",
+        "ciudades_principales": [
+            {"nombre": "Copenhague", "poblacion": 650000}
+        ],
+        "idiomas": {
+            "oficial": "Danés",
+            "otros_idiomas": "Groenlandés, feroés, alemán"
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional",
+            "jefe_estado": "Rey Federico X"
+        },
+        "fecha_adhesion": "1973-01-01"
+    },
+    {
+        "pais": "Eslovaquia",
+        "poblacion_nacional": 5450000,
+        "capital": "Bratislava",
+        "ciudades_principales": [
+            {"nombre": "Bratislava", "poblacion": 475000}
+        ],
+        "idiomas": {
+            "oficial": "Eslovaco",
+            "otros_idiomas": "Húngaro, romaní, ucranian"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Peter Pellegrini"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Eslovenia",
+        "poblacion_nacional": 2100000,
+        "capital": "Liubliana",
+        "ciudades_principales": [
+            {"nombre": "Liubliana", "poblacion": 295000}
+        ],
+        "idiomas": {
+            "oficial": "Esloveno",
+            "otros_idiomas": "Húngaro, italiano"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidenta Nataša Pirc Musar"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "España",
+        "poblacion_nacional": 49000000,
+        "capital": "Madrid",
+        "ciudades_principales": [
+            {"nombre": "Madrid", "poblacion": 3410000},
+            {"nombre": "Barcelona", "poblacion": 1700000},
+            {"nombre": "Valencia", "poblacion": 825948}
+        ],
+        "idiomas": {
+            "oficial": "Español (castellano)",
+            "otros_idiomas": "Catalán, valenciano, gallego, euskera, aranés"
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional",
+            "jefe_estado": "Rey Felipe VI"
+        },
+        "fecha_adhesion": "1986-01-01"
+    },
+    {
+        "pais": "Estonia",
+        "poblacion_nacional": 1310000,
+        "capital": "Tallin",
+        "ciudades_principales": [
+            {"nombre": "Tallin", "poblacion": 450000}
+        ],
+        "idiomas": {
+            "oficial": "Estonio",
+            "otros_idiomas": "Ruso"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Alar Karis"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Finlandia",
+        "poblacion_nacional": 5540000,
+        "capital": "Helsinki",
+        "ciudades_principales": [
+            {"nombre": "Helsinki", "poblacion": 660000}
+        ],
+        "idiomas": {
+            "oficial": "Finés, sueco",
+            "otros_idiomas": "Sami"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Alexander Stubb"
+        },
+        "fecha_adhesion": "1995-01-01"
+    },
+    {
+        "pais": "Francia",
+        "poblacion_nacional": 68000000,
+        "capital": "París",
+        "ciudades_principales": [
+            {"nombre": "París", "poblacion": 2200000},
+            {"nombre": "Marsella", "poblacion": 858120},
+            {"nombre": "Lyon", "poblacion": 506615}
+        ],
+        "idiomas": {
+            "oficial": "Francés",
+            "otros_idiomas": "bretón, corso, occitano"
+        },
+        "regimen_politico": {
+            "tipo": "República semipresidencialista",
+            "jefe_estado": "Presidente Emmanuel Macron"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Grecia",
+        "poblacion_nacional": 10420000,
+        "capital": "Atenas",
+        "ciudades_principales": [
+            {"nombre": "Atenas", "poblacion": 3100000}
+        ],
+        "idiomas": {
+            "oficial": "Griego",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidenta Katerina Sakellaropoulou"
+        },
+        "fecha_adhesion": "1981-01-01"
+    },
+    {
+        "pais": "Hungría",
+        "poblacion_nacional": 9660000,
+        "capital": "Budapest",
+        "ciudades_principales": [
+            {"nombre": "Budapest", "poblacion": 1700000}
+        ],
+        "idiomas": {
+            "oficial": "Húngaro",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Tamás Sulyok"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Irlanda",
+        "poblacion_nacional": 4930000,
+        "capital": "Dublín",
+        "ciudades_principales": [
+            {"nombre": "Dublín", "poblacion": 1400000}
+        ],
+        "idiomas": {
+            "oficial": "Irlandés, inglés",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Michael D. Higgins"
+        },
+        "fecha_adhesion": "1973-01-01"
+    },
+    {
+        "pais": "Italia",
+        "poblacion_nacional": 59000000,
+        "capital": "Roma",
+        "ciudades_principales": [
+            {"nombre": "Roma", "poblacion": 4330000},
+            {"nombre": "Milán", "poblacion": 1400000},
+            {"nombre": "Nápoles", "poblacion": 910000}
+        ],
+        "idiomas": {
+            "oficial": "Italiano",
+            "otros_idiomas": "Alemán, esloveno, francés, sardo, friulano"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Sergio Mattarella"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Letonia",
+        "poblacion_nacional": 1800000,
+        "capital": "Riga",
+        "ciudades_principales": [
+            {"nombre": "Riga", "poblacion": 600000}
+        ],
+        "idiomas": {
+            "oficial": "Letón",
+            "otros_idiomas": "Ruso"
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Edgars Rinkēvičs"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Lituania",
+        "poblacion_nacional": 2800000,
+        "capital": "Vilna",
+        "ciudades_principales": [
+            {"nombre": "Vilna", "poblacion": 550000}
+        ],
+        "idiomas": {
+            "oficial": "Lituano",
+            "otros_idiomas": "Polaco, ruso"
+        },
+        "regimen_politico": {
+            "tipo": "República semipresidencialista",
+            "jefe_estado": "Presidente Gitanas Nausėda"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Luxemburgo",
+        "poblacion_nacional": 670000,
+        "capital": "Luxemburgo",
+        "ciudades_principales": [
+            {"nombre": "Luxemburgo", "poblacion": 135000}
+        ],
+        "idiomas": {
+            "oficial": "Luxemburgués, francés, alemán",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional (Gran Ducado)",
+            "jefe_estado": "Gran Duque Enrique (Henri)"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Malta",
+        "poblacion_nacional": 540000,
+        "capital": "La Valeta",
+        "ciudades_principales": [
+            {"nombre": "La Valeta", "poblacion": 5800}
+        ],
+        "idiomas": {
+            "oficial": "Maltés, inglés",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Myriam Spiteri Debono"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Países Bajos",
+        "poblacion_nacional": 17990000,
+        "capital": "Ámsterdam",
+        "ciudades_principales": [
+            {"nombre": "Ámsterdam", "poblacion": 920000}
+        ],
+        "idiomas": {
+            "oficial": "Neerlandés",
+            "otros_idiomas": "Frisón, papiamento"
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional parlamentaria",
+            "jefe_estado": "Rey Guillermo Alejandro (Willem-Alexander)"
+        },
+        "fecha_adhesion": "1958-01-01"
+    },
+    {
+        "pais": "Polonia",
+        "poblacion_nacional": 36800000,
+        "capital": "Varsovia",
+        "ciudades_principales": [
+            {"nombre": "Varsovia", "poblacion": 1800000}
+        ],
+        "idiomas": {
+            "oficial": "Polaco",
+            "otros_idiomas": null
+        },
+        "regimen_politico": {
+            "tipo": "República parlamentaria",
+            "jefe_estado": "Presidente Andrzej Duda"
+        },
+        "fecha_adhesion": "2004-05-01"
+    },
+    {
+        "pais": "Portugal",
+        "poblacion_nacional": 10700000,
+        "capital": "Lisboa",
+        "ciudades_principales": [
+            {"nombre": "Lisboa", "poblacion": 545000},
+            {"nombre": "Oporto", "poblacion": 230000}
+        ],
+        "idiomas": {
+            "oficial": "Portugués",
+            "otros_idiomas": "Mirandés"
+        },
+        "regimen_politico": {
+            "tipo": "República semipresidencialista",
+            "jefe_estado": "Presidente Marcelo Rebelo de Sousa"
+        },
+        "fecha_adhesion": "1986-01-01"
+    },
+    {
+        "pais": "Rumanía",
+        "poblacion_nacional": 19070000,
+        "capital": "Bucarest",
+        "ciudades_principales": [
+            {"nombre": "Bucarest", "poblacion": 1800000}
+        ],
+        "idiomas": {
+            "oficial": "Rumano",
+            "otros_idiomas": "Húngaro, romaní, alemán"
+        },
+        "regimen_politico": {
+            "tipo": "República semipresidencialista",
+            "jefe_estado": "Presidente Klaus Iohannis"
+        },
+        "fecha_adhesion": "2007-01-01"
+    },
+    {
+        "pais": "Suecia",
+        "poblacion_nacional": 10500000,
+        "capital": "Estocolmo",
+        "ciudades_principales": [
+            {"nombre": "Estocolmo", "poblacion": 980000}
+        ],
+        "idiomas": {
+            "oficial": "Sueco",
+            "otros_idiomas": "Finlandés, sami, romaní, yiddish, meänkieli"
+        },
+        "regimen_politico": {
+            "tipo": "Monarquía constitucional parlamentaria",
+            "jefe_estado": "Rey Carlos XVI Gustavo (Carl XVI Gustaf)"
+        },
+        "fecha_adhesion": "1995-01-01"
+    }
+];
